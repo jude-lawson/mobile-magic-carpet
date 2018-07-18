46af26603de33cb4ecf269f9f32949ad9459712e
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-18 16:26:42 -0600

    Adds in messy but functional fetch method to button on landing page that updates state. Refactoring is definitely needed.'

--- App.js
+++ App.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import { Icon } from 'react-native-elements';
+import { Icon, Button } from 'react-native-elements';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -19,18 +19,49 @@ const instructions = Platform.select({
 
 // type Props = {};
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: 'This is the current body'
+    }
+    this.createAdventure = this.createAdventure.bind(this)
+  }
+
+  createAdventure() {
+   fetch('http://localhost:3000/api/v1/adventures', {
+     method: 'POST',
+     body: JSON.stringify({ name: 'An adventure' })
+   })
+    .then((response) => response.json())
+    .then((parsedResponse) => {
+      this.setState(() => ({
+        content: parsedResponse.message
+      }))
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
+      <View style={styles.div} />
         <Icon
           containerStyle={styles.settingsIcon}
           name='settings'
           size={40}
           onPress={() => alert('hello')} />
-        <Text 
-          style={styles.mainHeading}>
-          Magic Carpet
-        </Text>
+        <Button 
+          title='Magic Carpet'
+          buttonStyle={{
+            backgroundColor: '#ab37b6',
+            borderRadius: 50,
+            width: 200
+          }}
+          onPress={this.createAdventure} />
+          <Text>{this.state.content}</Text>
       </View>
     );
   }
@@ -41,15 +72,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#7998fe'
   },
   settingsIcon: {
     position: 'absolute',
     top: 40,
     right: 20
-  },
-  mainHeading: {
-    color: 'cornflowerblue',
-    fontSize: 20
   }
+  // div: {
+  //   backgroundColor: '#AADDFF',
+  //   width: 0,
+  //   height: 0,
+  //   backgroundColor: 'transparent',
+  //   borderRightWidth: 100,
+  //   borderRightColor: '#000000',
+  //   borderTopWidth: 100,
+  //   borderTopColor: '#000000',
+  //   borderLeftWidth: 50,
+  //   borderLeftColor: 'red',
+  //   borderBottomWidth: 50,
+  //   borderBottomColor: 'red',
+  //   position: 'absolute',
+  //   top: 0,
+  //   right: 0
+  // }
 });
