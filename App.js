ad57f80a0bac66edc12515185fb60ec2715fb954
Jude Lawson <lawsonjdutton@gmail.com>
2018-07-17 20:43:49 -0600

    Adds test settings icon to landing page

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Icon } from 'react-native-elements';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,6 +23,9 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text>Magic Carpet</Text>
+        <Icon
+          name='settings'
+          size={80} />
       </View>
     );
   }
